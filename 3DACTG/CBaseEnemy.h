b587d05397930b3b6ffa6e842da943b80b9a866a
dieghomj <diegomartj03@gmail.com>
2025-12-04 01:14:09 +0900

    Added base enemy behavior so that it reacts to spotlights, and also added global static  method to do vibration animations for static meshes

--- 3DACTG/CBaseEnemy.h
+++ 3DACTG/CBaseEnemy.h
@@ -2,6 +2,8 @@
 #include "CAnimCharacter.h"
 #include <vector>
 
+class CSpotLight;
+
 class CBaseEnemy :
 	public CAnimCharacter
 {
@@ -11,6 +13,7 @@ public:
 	enum enState {
 		Move,
 		Attack,
+		Retiring,
 		Dead
 	};
 
@@ -20,6 +23,7 @@ public:
 	~CBaseEnemy();
 
 	void Update();
+
 	void Draw(SCENE_DATA& sceneData);
 
 	void SetPath(std::vector<Pair> path);
@@ -28,8 +32,17 @@ public:
 
 	void SetTargetPlayer(CGameObject* player) { m_pPlayer = player; }
 
+	void ReactToSpotLight(const CSpotLight& spotLight);
+	bool IsDead() const { return m_State == Dead; }
+
 private:
 	void GetNextStep();
+	void AttackPlayer();
+	void IdleMove();
+	D3DXVECTOR3 SetRotationToTarget(D3DXVECTOR3 target);
+
+	bool IsLitBySpot(const CSpotLight& spotLight) const;
+	void ApplyRetreatAndDamage(float damage, float retreatForce);
 
 protected:
 
@@ -43,8 +56,14 @@ protected:
 	std::vector<Pair> m_StepList;
 	int m_pathStep;
 
+
+	float m_Health = 1000.f;
+	float m_RetreatTimer = 0.f;
+	float m_RetreatDuration = 2.f;
+
 	float moveSpeed;
 	float moveTime;
+	float m_fDistanceFromPlayer;
 
 	CGameObject* m_pPlayer;
 
